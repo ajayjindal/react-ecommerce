ecce19bef72068ff5750e40d209985547ed2a5ca
ajayjindal <ajay96jindal@gmail.com>
2021-11-12 00:47:41 +0530

    feat: added collection page and items

--- cuts-clothing/src/App.js
+++ cuts-clothing/src/App.js
@@ -1,17 +1,13 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 import { Route } from "react-router";
-const HatsPage = () => (
-    <div>
-        <h1>HATS PAGE</h1>
-    </div>
-);
 
 function App() {
     return (
         <div>
             <Route exact path="/" component={HomePage} />
-            <Route path="/shop/hats" component={HatsPage} />
+            <Route path="/shop" component={ShopPage} />
         </div>
     );
 }
