becb0a1ef717f98845aa0af4e0287561290f7148
ajayjindal <ajay96jindal@gmail.com>
2021-11-12 00:59:55 +0530

    feat: added header component

--- cuts-clothing/src/App.js
+++ cuts-clothing/src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-import { Route } from "react-router";
+import Header from "./components/header/header.component";
+import { Route, Switch } from "react-router";
 
 function App() {
     return (
         <div>
-            <Route exact path="/" component={HomePage} />
-            <Route path="/shop" component={ShopPage} />
+            <Header />
+            <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route path="/shop" component={ShopPage} />
+            </Switch>
         </div>
     );
 }
