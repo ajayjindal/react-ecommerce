a9ec9a61c1e61a419a5d7a8ee2c8d8582134d4c7
ajayjindal <ajay96jindal@gmail.com>
2021-11-24 23:50:24 +0530

    fix: firebase workflow

---
+++ src/firebase/firbase.cred.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDhqMMX5hHgr8LFdIiINbP8iUoAvaICXa0",
+    authDomain: "cuts-db.firebaseapp.com",
+    projectId: "cuts-db",
+    storageBucket: "cuts-db.appspot.com",
+    messagingSenderId: "1096401877611",
+    appId: "1:1096401877611:web:c9aae8ece3d13e86598f2e",
+};
+export default firebaseConfig;
