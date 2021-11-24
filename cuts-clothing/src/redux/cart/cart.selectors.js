790d61b92ff632426be38c7e0fe7c7a3ca2c4dae
ajayjindal <ajay96jindal@gmail.com>
2021-11-24 17:02:07 +0530

    feat: added memoized selectors

---
+++ cuts-clothing/src/redux/cart/cart.selectors.js
@@ -0,0 +1,13 @@
+import { createSelector } from "reselect";
+
+const selectCart = (state) => state.cart;
+// const selectUser = state => state.user;
+export const selectCartItems = createSelector(
+    [selectCart],
+    (cart) => cart.cartItems
+);
+export const selectCartItemsCount = createSelector(
+    [selectCartItems],
+    (cartItems) =>
+        cartItems.reduce((acc, cartItem) => acc + cartItem.quantity, 0)
+);
