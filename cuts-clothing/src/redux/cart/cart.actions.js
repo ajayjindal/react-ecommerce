91e0e2a6af7d74ac89e7f175a132112c499df0bb
ajayjindal <ajay96jindal@gmail.com>
2021-11-24 16:28:31 +0530

    feat: functionality to add products to cart state

--- cuts-clothing/src/redux/cart/cart.actions.js
+++ cuts-clothing/src/redux/cart/cart.actions.js
@@ -4,3 +4,9 @@ export const toggleCartHidden = () => {
         type: CartActionTypes.TOGGLE_CART_HIDDEN,
     };
 };
+export const addItem = (item) => {
+    return {
+        type: CartActionTypes.ADD_ITEM,
+        payload: item,
+    };
+};
